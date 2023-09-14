aeac8342680f7333669b970dc9123b2991526e0e
Meet <meetpatel51099@gmail.com>
2023-09-14 19:09:36 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:gst_calculator/Home_Screen.dart';
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "GST` Calculator",
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
